58101f43dd678159b1bbc09a35152b90faaf2880
Revankar <vidyarevankar22@gmail.com>
2025-12-05 18:22:05 +0530

    New Commit

--- test_functionality/test_suites.py
+++ test_functionality/test_suites.py
@@ -1,12 +1,17 @@
+import pytest
 from pages.newaccount import NewAccountPage
 
 class TestAmazon(NewAccountPage):
+
+    @pytest.mark.p1
     def test_login_invalidcredential(self):
         self.login_invalidcredential(email="vidyarevankar22@gmail.com",password='dont know')
 
+    @pytest.mark.p2
     def test_login_validcredential(self):
         self.login_validcredential(email="Admin",password='admin123')
 
+    @pytest.mark.p3
     def test_login_checkalert(self):
         self.login_validcredential(email="Admin", password='admin123')
 
